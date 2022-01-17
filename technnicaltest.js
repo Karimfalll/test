5a60e0bc59337c4238572a535065e0e9387a1873
Karimfalll <97653906+Karimfalll@users.noreply.github.com>
2022-01-17 06:49:07 +0200

    Update technnicaltest.js

--- technnicaltest.js
+++ technnicaltest.js
@@ -1,40 +1,29 @@
-const OriginalWords = (wordsArray,sentence,end,finalArray) => {
+function ArrangLikeInTheSentence(wordsArray, sentence) {
+  let finalArray = [];
+  let mesure = wordsArray.size;
 
-  if(finalArray.length > wordsArray.length || sentence === ""){return finalArray;}
-  
-  
-  else{
-  
-      while(end >= 0){
-       
-           for(let word of wordsArray){
-               let size = word.length;
-               let extractedWord = sentence.slice(0,size);
-             
-               if(extractedWord == word){
-                 
-                  finalArray.unshift(word);
-                  wordsArray = wordsArray.filter(function (f){return f !== word;});
-                  sentence = sentence.slice(size);
-                 
-                  OriginalWords(wordsArray,sentence,wordsArray.length,finalArray)
-        
-                }
-             
-               else{
-                  wordsArray = wordsArray.filter(function (f){return f !== word;});
-                  wordsArray.push(word);
-        
-                 OriginalWords(wordsArray,sentence,end-1,finalArray)
-                }
-                
-            }
-            
+  for (let i = 0; i <= mesure; i++) {
+    for (let word of wordsArray) {
+      let sizeWord = word.length;
+      if (sentence === '') {
+        return finalArray;
+      } else if (sizeWord > sentence.length) {
+        let NoInstinstriction =
+          'NO Instruction But IMPORTANT for the complexity';
+      } else {
+        let extractedWord = sentence.slice(0, sizeWord);
+        if (wordsArray.invcludes(extractedWord)) {
+          finalArray.unshift(word);
+          sentence = sentence.slice(sizeWord+1);
+        }
       }
-  
     }
+  }
+  if (sentence !== '') {
+    return [];
+  }
 
+  return finalArray;
 }
-
-let t = OriginalWords(['bien','pas','avance'],'avancebien',3,[]);
-console.log(t)
+let t = ArrangLikeInTheSentence(['bien', 'pas', 'avance'], 'avancebien');
+console.log(t);
